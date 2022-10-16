3a7dbf0c42f2c3229ee9a93c20bd19c5b4c0e082
ChengHSUHSU <ChengHSUHSU@gmail.com>
2022-10-16 09:58:12 +0000

    move consumer code to inep py

--- data_pipeline/dump_pipeline.py
+++ data_pipeline/dump_pipeline.py
@@ -1,89 +1,30 @@
 
 
 
-
-
-from util import load_config
 from producer import Producer
-from pyspark.sql import SparkSession
+from consumer import Consumer
 
 
 
 
 
-
-
-class Consumer:
-    def __init__(self, yaml_path=str):
-        # load config
-        self.cfg = load_config(yaml_path=yaml_path)
-
-        # Producer
-        self.producer_ = Producer(yaml_path=yaml_path)
-
-        self.available_data_paths = self.producer_.available_data_paths
-
-
-    def init_mongodb_spark_session(self,
-                                   mongodb_ip=None, 
-                                   table=None, 
-                                   collection=None, 
-                                   appName='myApp'):
-
-        # build mongo collection path
-        colleciton_path = "mongodb://{}/{}.{}".format(mongodb_ip,
-                                                      table,
-                                                      collection)
-        
-        # build session config
-        read_config = ["spark.mongodb.read.connection.uri", colleciton_path]
-        write_config = ["spark.mongodb.write.connection.uri", colleciton_path]
-        
-        # up session
-        mongodb_spark_session = SparkSession \
-                                .builder \
-                                .appName(appName) \
-                                .config(read_config[0], read_config[1]) \
-                                .config(write_config[0], write_config[1]) \
-                                .getOrCreate()
-        
-        return mongodb_spark_session
-
-    def write_to_db(self, data_info=dict, table=str, collection=str):
-
-        '''
-        data_info = {'data' : [(d1, d2, ...), (...), ....], 'columns' : [....]}
-        '''
-        # init spark session
-        mongodb_spark_session = self.init_mongodb_spark_session(self.cfg['mongodb_ip'], 
-                                                                table, 
-                                                                collection)
-
-        # build spark-dataframe
-        spark_dat = mongodb_spark_session.createDataFrame(data_info['data'], data_info['columns'])
-
-        
-        # submit spark-dataframe to mongoDB
-        spark_dat.write.format("mongodb").mode("append").save()
-
-
-
-
-
+# database
+table = 'test1'
+batch_size =  5000
 
 
 # yaml_path
 yaml_path = 'pipeline.yaml'
 
+
 # Producer
 producer = Producer(yaml_path)
+
 
 # Consumer
 consumer = Consumer(yaml_path)
 
-# database
-table = 'test1'
-batch_size = 10000
+
 
 
 for data_path in producer.available_data_paths:
@@ -114,7 +55,7 @@ for data_path in producer.available_data_paths:
             try:
                 consumer.write_to_db(data_info, table, collection)
             except Exception as error_message:
-                print(data_info)
+                #print(data_info)
                 print('=============================')
                 print(error_message)
                 print(len(data_info['data']))
@@ -128,8 +69,8 @@ for data_path in producer.available_data_paths:
     
     # check data_info
     if data_info['columns'] is not None:
-        consumer.write_to_db(data_info, table, collection)
-        continue
+        consumer.write_to_db(data_info, table, collection, True)
+    continue
 
 
 
