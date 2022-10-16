4e108ad859ec45032c3633066e05987293d82190
ChengHSUHSU <ChengHSUHSU@gmail.com>
2022-10-16 09:56:10 +0000

    replace json.loads(.) with eval(.)

--- data_pipeline/producer.py
+++ data_pipeline/producer.py
@@ -28,7 +28,8 @@ class Producer:
         columns = set()
         with open(json_path, 'r') as f:
             for record in f.readlines():
-                record = json.loads(record)
+                #record = json.loads(record)
+                record = eval(record)
                 columns = columns | set(record.keys())
         return list(columns)
 
@@ -36,9 +37,45 @@ class Producer:
     def build_generator(self, json_path=str):
         with open(json_path, 'r') as f:
             for record in f.readlines():
-                record = json.loads(record)
+                #record = json.loads(record)
+                record = eval(record)
                 yield record
     
+
+
+
+if __name__ == '__main__':
+    
+    yaml_path = 'pipeline.yaml'
+   
+    
+    producer = Producer(yaml_path)
+
+    available_data_paths = producer.available_data_paths
+
+
+    print('available_data_paths : ', available_data_paths)
+
+
+    columns = producer.show_all_columns(available_data_paths[0])
+
+    print('columns ; ', columns)
+
+
+    generator = producer.build_generator(available_data_paths[1])
+
+
+    for d in generator:
+        print(d)
+        quit()
+
+
+
+
+
+
+
+
 
 
 
